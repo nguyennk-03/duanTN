2737df84431f6c44838d00f7e51771591aef9a73
Nguyen <152572528+nguyennk-03@users.noreply.github.com>
2025-02-09 16:10:09 +0700

    danhmuc

--- StepViet/routes/api.php
+++ StepViet/routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\SanPhamController;
+use App\Http\Controllers\Api\DanhMucController;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +22,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::apiResource('/sanpham', SanPhamController::class);
+Route::apiResource('/danhmuc', DanhmucController::class);
